58d92e2f02a8eba96fe7fa10661ae1edc3660b11
mahabubimon <mahabubemon24@gmail.com>
2021-11-29 18:56:20 +0600

    banner added

---
+++ src/pages/home/banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./banner.css"
+
+const Banner = () => {
+    return (
+        <div className="banner-img">
+            <h2>Hello</h2>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/pages/home/home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Banner from "../banner/Banner";
+import Header from "../header/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <h2>Hello</h2>
+    </div>
+  );
+};
+
+export default Home;
